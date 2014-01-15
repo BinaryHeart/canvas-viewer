2781bb40bf31bcb9ebd46c5d28be7be7c9b9f7d3
Madelen <madelen.hermelin@gmail.com>
2014-01-15 08:53:59 +0100

    The assignment that are kind of finsish

---
+++ main.js
@@ -0,0 +1,40 @@
+
+    var canvas;
+    var canvas1;
+    var canvas3;
+    var canvas2;
+
+    var context;
+
+    function init() {
+
+        canvas = document.getElementById("myCanvas");
+
+        canvas1 = document.getElementById("canvasOne");
+        //canvas.setAttribute('width','1000');
+        canvas2 = document.getElementById("canvasTwo");
+        canvas3 = document.getElementById("canvasThree");
+
+        context = canvas.getContext("2d");
+
+        drawBox1();
+        drawBox2();
+        drawBox3();
+
+        canvas1.addEventListener('click', copy, false);
+        canvas2.addEventListener('click', copy, false);
+        canvas3.addEventListener('click', copy, false);
+    }
+
+    function copy(e) {
+
+
+      var destinationCanvas = document.getElementById(e.target.id);
+      context = canvas.getContext("2d");
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(destinationCanvas,0,0);
+      
+
+    }
+
+
---
+++ uppgift.js
@@ -0,0 +1,24 @@
+    function drawBox1() {
+     context = canvas1.getContext("2d");
+     context.fillRect(50, 25, 100, 100);
+
+     //Här skall ni implementera och rita första bilden.
+
+    }
+
+    function drawBox2() {
+     context = canvas2.getContext("2d");
+     context.fillRect(50, 25, 150, 100);
+
+     //Här skall ni implementera och rita andra bilden.
+
+
+    }
+
+    function drawBox3() {
+      context = canvas3.getContext("2d");
+     context.fillRect(50, 25, 150, 100);
+
+     //Här skall ni implementera och rita tredje bilden.
+
+    }
