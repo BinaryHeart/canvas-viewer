8b1875de580b238ce905e44d5e0ce741f891d698
Christopher Okhravi <christopher.okhravi@gmail.com>
2014-01-20 22:06:23 +0100

    Cleanup

--- uppgift.js
+++ uppgift.js
@@ -1,24 +1,20 @@
-    function drawBox1() {
-     context = canvas1.getContext("2d");
-     context.fillRect(50, 25, 100, 100);
+function drawBox1() {
+  context = canvas1.getContext("2d");
+  context.fillRect(50, 25, 100, 100);
 
-     //Här skall ni implementera och rita första bilden.
+  //Här skall ni implementera och rita första bilden.
+}
 
-    }
+function drawBox2() {
+  context = canvas2.getContext("2d");
+  context.fillRect(50, 25, 150, 100);
 
-    function drawBox2() {
-     context = canvas2.getContext("2d");
-     context.fillRect(50, 25, 150, 100);
+  //Här skall ni implementera och rita andra bilden.
+}
 
-     //Här skall ni implementera och rita andra bilden.
+function drawBox3() {
+  context = canvas3.getContext("2d");
+  context.fillRect(50, 25, 150, 100);
 
-
-    }
-
-    function drawBox3() {
-      context = canvas3.getContext("2d");
-     context.fillRect(50, 25, 150, 100);
-
-     //Här skall ni implementera och rita tredje bilden.
-
-    }
+  //Här skall ni implementera och rita tredje bilden.
+}
