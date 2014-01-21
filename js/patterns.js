d941c812b597640f3a0537ba36dc881e5a91e256
Christopher Okhravi <christopher.okhravi@gmail.com>
2014-01-21 11:32:15 +0100

    Use rounder numbers to make it easier working with the coordinates

--- js/patterns.js
+++ js/patterns.js
@@ -1,20 +1,20 @@
 function drawBox1(canvasElement) {
   context = canvasElement.getContext("2d");
-  context.fillRect(75, 75, 75, 75);
+  context.fillRect(50, 50, 50, 50);
 
   //Här skall ni implementera och rita första bilden.
 }
 
 function drawBox2(canvasElement) {
   context = canvasElement.getContext("2d");
-  context.fillRect(75, 75, 150, 150);
+  context.fillRect(50, 50, 150, 150);
 
   //Här skall ni implementera och rita andra bilden.
 }
 
 function drawBox3(canvasElement) {
   context = canvasElement.getContext("2d");
-  context.fillRect(150, 150, 75, 75);
+  context.fillRect(150, 150, 50, 50);
 
   //Här skall ni implementera och rita tredje bilden.
 }
